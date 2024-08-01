80e81175b05c2d3377eb532ecc9bcfbdc5561388
mltrev23 <trevor.dev23@gmail.com>
2024-08-01 13:04:16 -0400

    feat: select model type

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
 from fastapi.templating import Jinja2Templates
 from langchain_openai import ChatOpenAI
-from langchain_core.messages import HumanMessage, SystemMessage, AIMessage
+from langchain_core.messages import HumanMessage
 from dotenv import load_dotenv
 import os
 
@@ -19,18 +19,18 @@ templates = Jinja2Templates(directory="templates")
 async def read_root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
-chat_gpt4o = ChatOpenAI(api_key=api_key, model="gpt-4o", temperature=0)
-
 @app.post("/get_response")
 async def get_response(request: Request):
     data = await request.json()
     user_input = data.get('message')
+    model_type = data.get('model', 'gpt-4')  # Default to gpt-4 if not provided
+    
+    chat_gpt = ChatOpenAI(api_key=api_key, model=model_type, temperature=0)
+    
     messages = [
-        HumanMessage(
-            content=user_input
-        ),
+        HumanMessage(content=user_input),
     ]
-    ai_message = chat_gpt4o.invoke(messages)
+    ai_message = chat_gpt.invoke(messages)
     return JSONResponse(content={'response': ai_message.content})
 
 if __name__ == '__main__':
